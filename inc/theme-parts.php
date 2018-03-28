c7aa22f7b139a85b7b9333f315f9ea8557bb120a
Rachel Cherry <bamadesigner@gmail.com>
2018-03-27 23:03:15 -0700

    Moving Mailchimp signup to network plugin

--- inc/theme-parts.php
+++ inc/theme-parts.php
@@ -143,46 +143,6 @@ function wpcampus_2018_print_header_action() {
 add_action( 'wpc_add_before_body', 'wpcampus_2018_print_header_action', 1 );
 
 /**
- * Print the Mailchimp signup form.
- */
-function wpcampus_print_mailchimp_signup() {
-
-	?>
-	<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-	<div id="mc_embed_signup">
-		<form action="https://wpcampus.us11.list-manage.com/subscribe/post?u=6d71860d429d3461309568b92&amp;id=05f39a2a20" method="post" id="mc-embedded-subscribe-form" class="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-			<div id="mc_embed_signup_scroll">
-				<h2>Subscribe to WPCampus mailing list</h2>
-				<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-				<p>Sign-up to receive email updates about the WPCampus community and conference.</p>
-				<div class="mc-field-group-row name">
-					<div class="mc-field-group first-name">
-						<label for="mce-FNAME">First Name </label>
-						<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-					</div>
-					<div class="mc-field-group last-name">
-						<label for="mce-LNAME">Last Name </label>
-						<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
-					</div>
-				</div>
-				<div class="mc-field-group email">
-					<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
-					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-				</div>
-				<div id="mce-responses" class="clear">
-					<div class="response" id="mce-error-response" style="display:none"></div>
-					<div class="response" id="mce-success-response" style="display:none"></div>
-				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-				<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6d71860d429d3461309568b92_05f39a2a20" tabindex="-1" value=""></div>
-				<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-			</div>
-		</form>
-	</div>
-	<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[0]='EMAIL';ftypes[0]='email';fnames[6]='MMERGE6';ftypes[6]='radio';fnames[3]='MMERGE3';ftypes[3]='text';fnames[5]='MMERGE5';ftypes[5]='radio';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-	<?php
-}
-
-/**
  * Add the Mailchimp signup form to bottom of all content.
  */
 function wpcampus_2018_add_mailchimp_to_content( $content ) {
@@ -194,7 +154,9 @@ function wpcampus_2018_add_mailchimp_to_content( $content ) {
 
 	ob_start();
 
-	wpcampus_print_mailchimp_signup();
+	if ( function_exists( 'wpcampus_print_mailchimp_signup' ) ) {
+		wpcampus_print_mailchimp_signup();
+	}
 
 	return $content . ob_get_clean();
 
